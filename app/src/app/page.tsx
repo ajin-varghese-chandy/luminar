a3175560fb7798821aadad8ad6b6e83c6f56ea5d
aajinn <chandyajin@gmail.com>
2026-06-21 13:17:59 +0530

    REMOVED : NEXTJ APPLICATION COMPLEETLY

--- app/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getAllDays } from "@/lib/content"
-import { HomePageClient } from "./home-client"
-
-export const dynamic = "force-dynamic"
-
-export default async function HomePage() {
-  const days = await getAllDays()
-  return <HomePageClient days={days} />
-}
--- app/src/app/search/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getAllDays } from "@/lib/content"
-import { SearchPageClient } from "./search-client"
-
-export const dynamic = "force-dynamic"
-
-export default async function SearchPage() {
-  const days = await getAllDays()
-  return <SearchPageClient days={days} />
-}
